fc4c1af2c5b3e1af5ac39848e3fbae1b79eed33f
Isha Negi <itsishanegi@gmail.com>
2024-09-23 06:42:21 +0530

    Add files via upload

---
+++ passing-array-inside-a-function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#include<string.h>
+
+void str(char yn[])
+
+{  printf("your name is ");
+  
+puts(yn);
+
+}
+
+int main()
+
+{ char name[20];
+  
+printf("Enter name= ");
+  
+fgets(name,sizeof(name),stdin);
+  
+name[strcspn(name,"\n")]='\0';
+  
+str(name);
+    
+return 0;
+
+}
